06fd53f974a9643db0a01b065d4dfafc91a8990a
JaaumG <mail@joaoguilherme.dev>
2025-11-03 19:30:47 -0300

    feat(model.py): adiciona métodos para aplicar brilho, contraste e limiares na imagem

--- models/model.py
+++ models/model.py
@@ -25,6 +25,10 @@ class Model:
     def convert_to_gray(self):
         if self.image is None:
             return None
+
+        if len(self.image.shape) == 2:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGR)
+
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.image = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
         return self.to_tk_image(self.image)
@@ -32,9 +36,43 @@ class Model:
     def equalize_histogram(self):
         if self.image is None:
             return None
+
+        if len(self.image.shape) == 2:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGR)
+
         gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         equalized = cv2.equalizeHist(gray)
         self.image = cv2.cvtColor(equalized, cv2.COLOR_GRAY2BGR)
+        return self.to_tk_image(self.image)
+
+    def apply_brightness_contrast(self, brightness, contrast):
+        if self.image is None:
+            return None
+        self.image = cv2.convertScaleAbs(self.image, alpha=contrast, beta=brightness)
+        return self.to_tk_image(self.image)
+
+    def apply_global_threshold(self, threshold_value):
+        if self.image is None:
+            return None
+
+        if len(self.image.shape) == 2:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGR)
+
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
+        self.image = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+        return self.to_tk_image(self.image)
+
+    def apply_otsu_threshold(self):
+        if self.image is None:
+            return None
+
+        if len(self.image.shape) == 2:
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGR)
+
+        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        self.image = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
         return self.to_tk_image(self.image)
 
     # ========== Conversão ==========
